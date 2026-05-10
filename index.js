a8d1f396c03e629f90df4c6fc7c4ac5b5ea0fe99
Victor Manuel <victor.h.cc.2006@gmail.com>
2026-05-10 14:58:53 -0500

    Fix express server

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 require("dotenv").config();
 
-console.log("Servidor funcionando");
-console.log("Puerto:", process.env.PORT);
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Servidor funcionando en Render");
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor activo en puerto ${PORT}`);
+});
